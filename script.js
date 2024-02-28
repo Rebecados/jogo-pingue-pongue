7d3b96f4a86710e085097a89ed5704960fa57268
Rebecados <160244451+Rebecados@users.noreply.github.com>
2024-02-28 09:00:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let xBolinha = 400
+let yBolinha = 250
+let diametrto = 30 
+
+
+
+
+
+function setup() {  createCanvas(800, 500);
+}
+
+function draw() {
+  background(0);
+
+  circle(xBolinha,yBolinha, diametrto);
+}
+
